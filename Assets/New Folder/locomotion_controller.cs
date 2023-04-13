26294be76681307ef0858ccd248d6b1c5730f43b
janhachoi <wnsgk7654@gmail.com>
2023-04-14 04:48:35 +0900

    4-14

--- Assets/New Folder/locomotion_controller.cs
+++ Assets/New Folder/locomotion_controller.cs
@@ -10,15 +10,27 @@ public class locomotion_controller : MonoBehaviour
     public InputHelpers.Button teleport_activation_button;
     public float activation_threshold = 0.1f;
 
+    public XRRayInteractor left_interactor_ray;
+    public XRRayInteractor right_interactor_ray;
+
     public bool enable_left_teleport { get; set; } = true;
-    //public bool enable_right_teleport = true;
+    //public bool enable_right_teleport { get; set; }= true;
 
 
     void Update()
     {
+        Vector3 pos = new Vector3();
+        Vector3 normal = new Vector3();
+        int index = 0;
+        bool valid_target = false;
+
+
         if (left_teleport_ray)
         {
-            left_teleport_ray.gameObject.SetActive(enable_left_teleport && check_if_activated(left_teleport_ray));
+            bool is_left_interactor_ray_hovering = left_interactor_ray.TryGetHitInfo(out pos, out normal, out index, out valid_target);
+            left_teleport_ray.gameObject.SetActive(enable_left_teleport &&
+                check_if_activated(left_teleport_ray) &&
+                !is_left_interactor_ray_hovering);
         }
     }
 
