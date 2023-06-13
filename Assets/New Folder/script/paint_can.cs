bbeb97e2259a5db5f0f7c0c66a79b166513adbd3
janhachoi <wnsgk7654@gmail.com>
2023-06-14 00:48:03 +0900

    6-14

--- Assets/New Folder/script/paint_can.cs
+++ Assets/New Folder/script/paint_can.cs
@@ -10,21 +10,32 @@ public class paint_can : MonoBehaviour
     private void Start()
     {
         mesh_r = GetComponent<MeshRenderer>();
+        gameObject.layer = 14;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("paint_drump_red"))
         {
             mesh_r.material = mat[0];
+            gameObject.layer = 13;
         }
         else if (other.CompareTag("paint_drump_green"))
         {
             mesh_r.material = mat[1];
+            gameObject.layer = 13;
+
         }
         else if(other.CompareTag("paint_drump_blue"))
         {
             mesh_r.material = mat[2];
+            gameObject.layer = 13;
+        }
+        else if (other.CompareTag("paint_drump_white"))
+        {
+            mesh_r.material = mat[3];
+            gameObject.layer = 13;
         }
     }
 }
