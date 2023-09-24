08a6d74e44f0e0fbbc6320d1464b1f97f1b3da44
janhachoi <wnsgk7654@gmail.com>
2023-09-25 01:26:08 +0900

    9-25 add particles

---
+++ Assets/New Folder/script/grabbables/water_on.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class water_on : MonoBehaviour
+{
+
+    public GameObject switch_pos;
+    public float _off = 0.2f;
+
+    public GameObject particle_obj;
+
+    bool is_touch;
+    private void Update()
+    {
+        if (is_touch)
+        {
+            Vector3 rotation = switch_pos.transform.rotation.eulerAngles;
+            if (rotation.x <= _off)
+            {
+                particle_obj.SetActive(false);
+            }
+            else
+            {
+                particle_obj.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Untagged"))
+        {
+            is_touch = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Untagged"))
+        {
+            is_touch = false;
+        }
+    }
+}
