dc992df002572bf6ec38818d6075d7402ea60264
janhachoi <wnsgk7654@gmail.com>
2023-10-18 04:09:34 +0900

    10-18 Add effects asset and implement box open

--- Assets/New Folder/script/paint/paint_can.cs
+++ Assets/New Folder/script/paint/paint_can.cs
@@ -14,8 +14,6 @@ public class paint_can : MonoBehaviour
     {
         mesh_r = GetComponent<MeshRenderer>();
         skinned_mech_r =GetComponent<SkinnedMeshRenderer>();
-
-        gameObject.layer = 10;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,47 +25,13 @@ public class paint_can : MonoBehaviour
             else if(skinned_mech_r) { skinned_mech_r.material = mat[0]; }
 
             collision_r.paintColor = mat[0].color;
-            //gameObject.layer = 13;
         }
-        //else if (other.CompareTag("paint_drump_green"))
-        //{
-        //    if (mesh_r) { mesh_r.material = mat[1]; }
-        //    else if (skinned_mech_r) { skinned_mech_r.material = mat[1]; }
-
-        //    collision_r.paintColor = mat[1].color;
-        //    gameObject.layer = 13;
-        //}
-        //else if(other.CompareTag("paint_drump_blue"))
-        //{
-        //    if (mesh_r) { mesh_r.material = mat[2]; }
-        //    else if (skinned_mech_r) { skinned_mech_r.material = mat[2]; }
-
-        //    collision_r.paintColor = mat[2].color;
-        //    gameObject.layer = 13;
-        //}
-        //else if (other.CompareTag("paint_drump_white"))
-        //{
-        //    if (mesh_r) { mesh_r.material = mat[3]; }
-        //    else if (skinned_mech_r) { skinned_mech_r.material = mat[3]; }
-
-        //    collision_r.paintColor = mat[3].color;
-        //    gameObject.layer = 13;
-        //}
-        //else if (other.CompareTag("paint_drump_black"))
-        //{
-        //    if (mesh_r) { mesh_r.material = mat[4]; }
-        //    else if (skinned_mech_r) { skinned_mech_r.material = mat[4]; }
-
-        //    collision_r.paintColor = mat[4].color;
-        //    gameObject.layer = 13;
-        //}
-        else if (other.CompareTag("paint_drump_erase"))//5
+        else if (other.CompareTag("paint_drump_erase"))
         {
-            if (mesh_r) { mesh_r.material = mat[1]; }
-            else if (skinned_mech_r) { skinned_mech_r.material = mat[1]; }
+            if (mesh_r) { mesh_r.material = mat[2]; } //basic color
+            else if (skinned_mech_r) { skinned_mech_r.material = mat[2]; }
 
             collision_r.paintColor = mat[1].color;
-            //gameObject.layer = 13;
         }
     }
 
