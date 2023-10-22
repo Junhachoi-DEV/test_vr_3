2639b820e0779b8c03d2b8c116969dc14987fffc
janhachoi <wnsgk7654@gmail.com>
2023-10-23 05:31:10 +0900

    10-23 implement chair

--- Assets/New Folder/script/chair/chair.cs
+++ Assets/New Folder/script/chair/chair.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class chair : MonoBehaviour
 {
     public GameObject[] chair_parts;
+    public GameObject[] next_chir_parts;
+
 
     int part_num;
+    public int count_num;
+
+    public bool is_touch;
+
+    public void chair_part_active(bool value)
+    {
+        if (count_num < chair_parts.Length-1)
+        {
+            next_chir_parts[count_num].SetActive(value);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("chair_part"))
+        if (other.CompareTag("chair_part")&& is_touch)
         {
             part_num = other.GetComponent<chair_part>().part_num;
-            chair_parts[part_num].SetActive(true);
+            if (part_num == count_num)
+            {
+                chair_parts[part_num].SetActive(true);
+                next_chir_parts[part_num].SetActive(false);
+                if (count_num < chair_parts.Length-1) { count_num++; } else { return; }
+                next_chir_parts[count_num].SetActive(true);
+            }
         }
+    }
+
+
+    public void is_touch_bool(bool value)
+    {
+        is_touch = value;
     }
 }
--- Assets/New Folder/script/chair/chair_part.cs
+++ Assets/New Folder/script/chair/chair_part.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class chair_part : MonoBehaviour
 {
     public int part_num;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("chair_main"))
+        {
+            chair _chair = other.GetComponentInParent<chair>();
+            if (_chair.is_touch && _chair.count_num == part_num)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    
 }
