25241e2df56bbe8c51848f55915e13ca35e16926
janhachoi <wnsgk7654@gmail.com>
2023-10-30 03:35:53 +0900

    10-30 implement tele hand change

--- Assets/New Folder/script/locomotion_controller.cs
+++ Assets/New Folder/script/locomotion_controller.cs
@@ -4,7 +4,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class locomotion_controller : MonoBehaviour
 {
     public XRController left_teleport_ray;
-    //public XRController right_teleport_ray;
+    public XRController right_teleport_ray;
     public InputHelpers.Button teleport_activation_button;
     public float activation_threshold = 0.1f;
 
@@ -12,7 +12,9 @@ public class locomotion_controller : MonoBehaviour
     public XRRayInteractor right_interactor_ray;
 
     public bool enable_left_teleport { get; set; } = true;
-    //public bool enable_right_teleport { get; set; }= true;
+    public bool enable_right_teleport { get; set; }= true;
+
+    public bool is_hand_change;
 
     void Update()
     {
@@ -21,20 +23,30 @@ public class locomotion_controller : MonoBehaviour
         int index = 0;
         bool valid_target = false;
 
-
-        if (left_teleport_ray)
+        if (left_teleport_ray && !is_hand_change)
         {
             bool is_left_interactor_ray_hovering = left_interactor_ray.TryGetHitInfo(out pos, out normal, out index, out valid_target);
             left_teleport_ray.gameObject.SetActive(enable_left_teleport &&
                 check_if_activated(left_teleport_ray) &&
                 !is_left_interactor_ray_hovering);
         }
-
+        else if (right_teleport_ray && is_hand_change)
+        {
+            bool is_right_interactor_ray_hovering = right_interactor_ray.TryGetHitInfo(out pos, out normal, out index, out valid_target);
+            right_teleport_ray.gameObject.SetActive(enable_right_teleport &&
+                check_if_activated(right_teleport_ray) &&
+                !is_right_interactor_ray_hovering);
+        }
     }
 
     public bool check_if_activated(XRController controller)
     {
         InputHelpers.IsPressed(controller.inputDevice, teleport_activation_button, out bool is_actiated, activation_threshold);
         return is_actiated;
+    }
+
+    public void is_hand_changer(bool value)
+    {
+        is_hand_change = value;
     }
 }
