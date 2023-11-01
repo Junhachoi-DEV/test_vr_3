918064483c71c8276a4af74b63417d8d2a5fe7f9
janhachoi <wnsgk7654@gmail.com>
2023-11-02 05:01:03 +0900

    11-2 implement tutorial scene

---
+++ Assets/New Folder/script/tutorial/tutorial.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject xr_origin;
+    public GameObject goal_ef;
+    public GameObject tele_area;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject start_icon;
+    public AudioSource goal_sfx;
+    public GameObject[] ui_s;
+
+}
---
+++ Assets/New Folder/script/tutorial/tutorial_goal.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using static UnityEngine.Rendering.DebugUI;
+
+public class tutorial_goal : MonoBehaviour
+{
+    tutorial _tutorial;
+    int num;
+    bool is_touch;
+
+    private void Awake()
+    {
+        num = 0;
+        _tutorial = FindObjectOfType<tutorial>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !is_touch)
+        {
+            _tutorial.xr_origin.transform.position = Vector3.zero;
+            _tutorial.goal_sfx.Play();
+            tuoched_check(num);
+            num++;
+            is_touch = true;
+            Invoke("cool_time", 0.2f);
+        }
+    }
+    void cool_time()
+    {
+        is_touch = false;
+    }
+    void tuoched_check(int value)
+    {
+        _tutorial.ui_s[value+1].SetActive(true);
+
+
+        switch (value)
+        {
+            case 0:
+                _tutorial.xr_origin.GetComponent<TeleportationProvider>().enabled = true;
+                _tutorial.xr_origin.GetComponent<locomotion_controller>().enabled = true;
+                _tutorial.tele_area.SetActive(true);
+                break;
+            case 1:
+                _tutorial.tele_area.SetActive(false);
+                _tutorial.cube1.SetActive(true);
+                _tutorial.cube2.SetActive(true);
+                break;
+            case 2:
+                _tutorial.cube1.SetActive(false);
+                _tutorial.cube2.SetActive(false);
+                _tutorial.xr_origin.GetComponent<player_ui_controller>().enabled = true;
+                break;
+            case 3:
+                _tutorial.start_icon.SetActive(true);
+                break;
+        }
+
+    }
+}
